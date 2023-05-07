db01fb892b6e46a9100e02c170a88e41b75b35ee
Bruno <bruno142133@gmail.com>
2023-05-07 20:00:31 -0300

    Co-authored-by: herokaio321123@gmail.com <herokaio321123@gmail.com>

---
+++ screens/WelcomeScreen.tsx
@@ -0,0 +1,127 @@
+import {
+  Dimensions,
+  ImageBackground,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React from "react";
+import Spacing from "../constants/Spacing";
+import FontSize from "../constants/FontSize";
+import Colors from "../constants/Colors";
+import Font from "../constants/Font";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { RootStackParamList } from "../types";
+const { height } = Dimensions.get("window");
+
+type Props = NativeStackScreenProps<RootStackParamList, "Welcome">;
+
+const WelcomeScreen: React.FC<Props> = ({ navigation: { navigate } }) => {
+  return (
+    <SafeAreaView>
+      <View>
+        <ImageBackground
+          style={{
+            marginTop: Spacing * 15,
+            height: height / 4,
+            borderRadius: 200 / 2
+          }}
+          resizeMode="contain"
+          source={require("../assets/images/libras.png")}
+        />
+        <View
+          style={{
+            paddingHorizontal: Spacing * 4,
+            paddingTop: Spacing * 4,
+          }}
+        >
+          <Text
+            style={{
+              fontSize: FontSize.xxLarge,
+              color: Colors.primary,
+              fontFamily: Font["poppins-bold"],
+              textAlign: "center",
+            }}
+          >
+            LibrasApp
+          </Text>
+
+          <Text
+            style={{
+              fontSize: FontSize.small,
+              color: Colors.text,
+              fontFamily: Font["poppins-regular"],
+              textAlign: "center",
+              marginTop: Spacing * 2,
+            }}
+          >
+            Aprenda Libras e faça o mundo ser um lugar mais acessível.
+          </Text>
+        </View>
+        <View
+          style={{
+            paddingHorizontal: Spacing * 2,
+            paddingTop: Spacing * 6,
+            flexDirection: "row",
+          }}
+        >
+          <TouchableOpacity
+            onPress={() => navigate("Login")}
+            style={{
+              backgroundColor: Colors.primary,
+              paddingVertical: Spacing * 1.5,
+              paddingHorizontal: Spacing * 2,
+              width: "48%",
+              borderRadius: Spacing,
+              shadowColor: Colors.primary,
+              shadowOffset: {
+                width: 0,
+                height: Spacing,
+              },
+              shadowOpacity: 0.3,
+              shadowRadius: Spacing,
+            }}
+          >
+            <Text
+              style={{
+                fontFamily: Font["poppins-bold"],
+                color: Colors.onPrimary,
+                fontSize: FontSize.large,
+                textAlign: "center",
+              }}
+            >
+              Login
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => navigate("Register")}
+            style={{
+              paddingVertical: Spacing * 1.5,
+              paddingHorizontal: Spacing * 2,
+              width: "48%",
+              borderRadius: Spacing,
+              borderColor: Colors.primary
+            }}
+          >
+            <Text
+              style={{
+                fontFamily: Font["poppins-bold"],
+                color: Colors.text,
+                fontSize: FontSize.large,
+                textAlign: "center",
+              }}
+            >
+              Registrar
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default WelcomeScreen;
+
+const styles = StyleSheet.create({});
